3d9d7e3659c8731197c3cb89c7235a5676ba04e2
Harrison-S1 <disco_dingo@outlook.com>
2022-11-02 14:18:47 +0000

    concouse update

--- aws_dev_env_concourse/main.tf
+++ aws_dev_env_concourse/main.tf
@@ -74,7 +74,7 @@ resource "aws_security_group" "main_aws_sg" {
 resource "aws_instance" "dev_node" {
   instance_type          = "t2.micro"
   ami                    = data.aws_ami.server_ami.id
-  key_name               = aws_key_pair.clouddev_auth.id
+  # key_name               = aws_key_pair.clouddev_auth.id
   vpc_security_group_ids = [aws_security_group.main_aws_sg.id]
   iam_instance_profile   = aws_iam_instance_profile.dev_profile.name
   subnet_id              = aws_subnet.main_public_subnet.id
